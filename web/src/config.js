074cc0c615a39d499539d6be37293fe54aac4acf
Muhaajir Benjamin Sayer <muhaajirsayer@Muhaajirs-MBP.fios-router.home>
2020-12-09 12:27:23 -0500

    git init

---
+++ web/src/config.js
@@ -0,0 +1,6 @@
+export default {
+  // Deployment automation script
+  API_ENDPOINT: 'https://felix-assistant.herokuapp.com/api',
+  TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY || 'felix-felicis' // Temporary
+}
+
