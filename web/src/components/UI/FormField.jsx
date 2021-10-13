92b4997a8689740fd482bed8621fcf7eeec0cb0a
muhajir <haji.san@icloud.com>
2021-10-13 08:08:18 -0400

    fix initial values logic

--- web/src/components/UI/FormField.jsx
+++ web/src/components/UI/FormField.jsx
@@ -4,7 +4,7 @@ import { TextField } from '@material-ui/core';
 
 export const FormField = ({formik, name, ...other}) => {
   const { values, touched, errors, handleChange } = formik
-
+  console.log(values)
   return (
     <TextField
       value={values[name]}
